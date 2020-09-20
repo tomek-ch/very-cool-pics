60e1edea81e2ae072007b4115934c0afed5a3747
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2020-09-20 15:41:37 +0200

    Lifted user profile state up from UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,30 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 
-import { db } from '../firebase';
-import { Context } from '../Context';
+
 import SignOutButton from './SignOutButton';
 
-function UserInfo() {
-    
-    const { currentUser } = useContext(Context);
-    const { username } = useParams();
-    const isThisCurrentUsersProfile = currentUser.username === username;
-
-    const [ user, setUser ] = useState({});
-
-    useEffect(() => {
-        const getUser = async () => {
-
-            setUser((await db
-                .collection('Users')
-                .where('username', '==', username)
-                .get()).docs[0].data());
-        }
-        getUser();
-    }, [username]);
-
-    
+function UserInfo({ isThisCurrentUsersProfile, username }) {
     return (
         <div className="user-info">
             <div className="info-profile-pic">
@@ -34,7 +13,7 @@ function UserInfo() {
                 />
             </div>
             <div className="info-username">
-                {user.username}
+                {username}
             </div>
             {isThisCurrentUsersProfile ? <SignOutButton /> : ''}
         </div>
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { db } from '../firebase';
+import { Context } from '../Context';
 import UserInfo from '../components/UserInfo';
 
 function UserProfile() {
+    
+    const { currentUser } = useContext(Context);
+    const { username } = useParams();
+    const isThisCurrentUsersProfile = currentUser.username === username;
+
+    // const [ user, setUser ] = useState({});
+
+    // useEffect(() => {
+    //     const getUser = async () => {
+
+    //         setUser((await db
+    //             .collection('Users')
+    //             .where('username', '==', username)
+    //             .get()).docs[0].data());
+    //     }
+    //     getUser();
+    // }, [username]);
+
+    
     return (
-        <div className="user-profile">
-            <UserInfo />
+        <div className="user-profile" >
+            <UserInfo
+                isThisCurrentUsersProfile={isThisCurrentUsersProfile}
+                username={username}
+            />
         </div>
     );
 }
