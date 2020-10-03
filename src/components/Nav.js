19fbc0f8d4ec70e1c605cf3352f2713c1056bf84
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2020-10-03 20:43:50 +0200

    Added nav icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,26 +2,31 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Context } from '../Context';
+import home from '../icons/home.svg';
+import search from '../icons/search.svg';
+import plusSquare from '../icons/plus-square.svg';
+import heart from '../icons/heart.svg';
+import user from '../icons/user.svg';
 
 function Nav() {
     const { currentUser, notifications } = useContext(Context);
     
     return (
         <nav className="nav-bar">
-            <Link to="/">
-                Feed
+            <Link to="/" className="nav-link">
+                <img src={home} alt="" />
             </Link>
-            <Link to="/explore">
-                Explore
+            <Link to="/explore" className="nav-link">
+                <img src={search} alt="" />
             </Link>
-            <Link to="/new-post">
-               Add
+            <Link to="/new-post" className="nav-link">
+               <img src={plusSquare} alt="" />
             </Link>
-            <Link to="/notifications">
-               Notifications ({notifications.filter(not => not.unread).length})
+            <Link to="/notifications" className="nav-link">
+               <img src={heart} alt="" /> ({notifications.filter(not => not.unread).length})
             </Link>
-            <Link to={`/${currentUser.username}`}>
-                Me
+            <Link to={`/${currentUser.username}`} className="nav-link">
+                <img src={user} alt="" />
             </Link>
         </nav>
     );
