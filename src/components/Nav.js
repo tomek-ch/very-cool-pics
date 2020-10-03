399978d4ab1a211c703b883616d4a80de4c02ad0
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2020-10-03 21:00:45 +0200

    Added active class to nav icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import { Context } from '../Context';
@@ -9,23 +10,24 @@ import heart from '../icons/heart.svg';
 import user from '../icons/user.svg';
 
 function Nav() {
-    const { currentUser, notifications } = useContext(Context);
+    const { currentUser: { username }, notifications } = useContext(Context);
+    const path = useLocation().pathname;
     
     return (
         <nav className="nav-bar">
-            <Link to="/" className="nav-link">
+            <Link to="/" className={`nav-link ${path === '/' ? 'active-link' : ''}`}>
                 <img src={home} alt="" />
             </Link>
-            <Link to="/explore" className="nav-link">
+            <Link to="/explore" className={`nav-link ${path === '/explore' ? 'active-link' : ''}`}>
                 <img src={search} alt="" />
             </Link>
-            <Link to="/new-post" className="nav-link">
+            <Link to="/new-post" className={`nav-link ${path === '/new-post' ? 'active-link' : ''}`}>
                <img src={plusSquare} alt="" />
             </Link>
-            <Link to="/notifications" className="nav-link">
+            <Link to="/notifications" className={`nav-link ${path === '/notifications' ? 'active-link' : ''}`}>
                <img src={heart} alt="" /> ({notifications.filter(not => not.unread).length})
             </Link>
-            <Link to={`/${currentUser.username}`} className="nav-link">
+            <Link to={`/${username}`} className={`nav-link ${path === `/${username}` ? 'active-link' : ''}`}>
                 <img src={user} alt="" />
             </Link>
         </nav>
