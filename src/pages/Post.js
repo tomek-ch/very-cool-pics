ce2ffaf080ecc2e938ed036ad6e56fe21816a04c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2020-10-13 15:48:14 +0200

    Add loading state to post page

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -4,10 +4,12 @@ import { useParams, useHistory } from 'react-router-dom';
 import { db } from '../firebase';
 import PostSection from '../components/post/PostSection';
 import NotFound from './NotFound';
+import Loading from '../components/Loading';
 
 function Post() {
     const { postId } = useParams();
     const [ post, setPost ] = useState(null);
+    const [ isLoading, setIsLoading ] = useState(true);
     
     const history = useHistory();    
     const handleDelete = () => {
@@ -17,10 +19,15 @@ function Post() {
     useEffect(() => {
         db.collection('Posts').doc(postId).get().then(doc => {
             setPost(doc.data());
+            setIsLoading(false);
         });
     }, [postId]);
 
     return (
+        isLoading
+        ?
+        <Loading />
+        :
         post
         ?
         <PostSection
