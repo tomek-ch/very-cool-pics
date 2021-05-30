721509e2948afdc235eddf82a91ae40deaf8e1f8
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-30 21:08:39 +0200

    Restrict nav width

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,40 +7,43 @@ import { Context } from '../Context';
 function Nav() {
     const { currentUser: { username }, notifications } = useContext(Context);
     const path = useLocation().pathname;
-    
+
     const unreadNumber = notifications.filter(not => not.unread).length;
 
     return (
         <nav className="nav-bar">
-            <Link to="/" className="nav-link">
-                {path === '/' ? <i className="ri-home-2-fill"></i> : <i className="ri-home-2-line"></i>}
-            </Link>
-            <Link to="/explore" className="nav-link">
-                {path === '/explore' ? <i className="ri-compass-3-fill"></i> : <i className="ri-compass-3-line"></i>}
-            </Link>
-            <Link to="/new-post" className="nav-link">
-                {path === '/new-post' ? <i className="ri-add-box-fill"></i> : <i className="ri-add-box-line"></i>}
-            </Link>
-            <Link to="/notifications" className="nav-link">
-                <div className="notifications-icon">
-                    {path === '/notifications' ? <i className="ri-heart-3-fill"></i> : <i className="ri-heart-3-line"></i>}
-                    {
+            <div className="nav-inner">
+                <Link to="/" className="nav-link">
+                    {path === '/' ? <i className="ri-home-2-fill"></i> : <i className="ri-home-2-line"></i>}
+                </Link>
+                <Link to="/explore" className="nav-link">
+                    {path === '/explore' ? <i className="ri-compass-3-fill"></i> : <i className="ri-compass-3-line"></i>}
+                </Link>
+                <Link to="/new-post" className="nav-link">
+                    {path === '/new-post' ? <i className="ri-add-box-fill"></i> : <i className="ri-add-box-line"></i>}
+                </Link>
+                <Link to="/notifications" className="nav-link">
+                    <div className="notifications-icon">
+                        {path === '/notifications' ? <i className="ri-heart-3-fill"></i> : <i className="ri-heart-3-line"></i>}
+                        {
 
-                    }
-                    {
-                        unreadNumber
-                        ?
-                        <div className="notifications-badge">
-                            {unreadNumber}
-                        </div>
-                        :
-                        ''
-                    }
-                </div>
-            </Link>
-            <Link to={`/${username}`} className="nav-link">
-                {path === `/${username}` ? <i className="ri-user-fill"></i> : <i className="ri-user-line"></i>}
-            </Link>
+                        }
+                        {
+                            unreadNumber
+                                ?
+                                <div className="notifications-badge">
+                                    {unreadNumber}
+                                </div>
+                                :
+                                ''
+                        }
+                    </div>
+                </Link>
+                <Link to={`/${username}`} className="nav-link">
+                    {path === `/${username}` ? <i className="ri-user-fill"></i> : <i className="ri-user-line"></i>}
+                </Link>
+
+            </div>
         </nav>
     );
 }
